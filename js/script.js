37c4f6cd9effc9082bf144b530fc22f22e9b0195
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-03-01 12:49:40 +0100

    styled the css for mobile app and made the input functions for the first step

--- js/script.js
+++ js/script.js
@@ -1,71 +1,77 @@
-//the div that's wrapping the whole document
-const wrapperDiv = document.getElementById("wrapper")
-//the aside side bar
-const aside = document.getElementById("aside")
-//the sidebar step buttons
-let stepButtonOne = document.getElementById("step-btn-one")
-let stepButtonTwo = document.getElementById("step-btn-two")
-let stepButtonThree = document.getElementById("step-btn-three")
-let stepButtonFour = document.getElementById("step-btn-four")
-//the form slides
-let slidesForm = document.getElementById("slides-form")
-const form = document.getElementById("form")
-//the next buttons to each form slide
-let nextBtn = document.getElementById("next-btn")
-//the babk buttons to each slide
-let backBtn = document.getElementById("back-btn")
-//the confirm buttons
-const confirmButton = document.getElementById("confirm-btn")
-
-
-let userName;
-let email;
-let phone;
-
-document.getElementById("submit-btn-username").value = function () {
-userName = document.getElementById("username").value 
-
+function goToStepTwo(){
+    checkName()
+    checkPhoneNumber()
+    checkMail()
+    Achieve()
 }
 
-document.getElementById("submit-btn-email").value = function () {
-email = document.getElementById("email").value 
+function checkName(){
+    //id tag for for input field for name
+    let myNameInput = document.getElementById("infoName")
+    //span id "errorName"
+    let myNameError = document.getElementById("errorName")
+    //
+    let myNameRegex = /^[a-zA-Z-\s]+$/;
+
+    //removing the white parts
+    if(myNameInput.value.trim()== "") {
+        //displays the error message
+        myNameError.innerHTML = "This field is required";
+        //if the value the user is putting in fails myNameRegex
+    } else if(myNameRegex.test(myNameInput.value)== false){
+        //display this error message
+        myNameError.innerHTML = "Please enter a valid name";
+        //if the value that the user is putting in isn't a string
+    } else if(myNameInput.value.trim() !== "") {
+        //make it a string
+        myNameError.innerHTML = "";
+    }
 }
 
-document.getElementById("submit-btn-phone").value = function () {
-phone = document.getElementById("phone").value 
+function checkPhoneNumber(){
+    //id tag for for input field for phone number
+    let myInputNum = document.getElementById("infoPhoneNumber")
+    //the span id
+    let myErrorNum = document.getElementById("errorPhoneNumber")
+    //The number boundaries that the user can't cross
+    let myRegexNum = /^[0-9\s]+$/;
+
+    //if the value is a string after it's trimmed
+    if(myInputNum.value.trim()==""){
+        //display this string
+        myErrorNum.innerHTML = "This field is required";
+        //if the value the user puts in fails the regex test
+    } else if(myRegexNum.test(myInputNum.value) == false){
+        //display this string
+        myErrorNum.innerHTML = "Enter number values"
+    } else if(myInputNum.value.trim().length !== 10){
+        myErrorNum.innerHTML = "Enter valid phone number"
+    } else {
+        myErrorNum.innerHTML = "";
+    }
 }
 
-let stepCount = 0;
-let selectorCount = 1;
-let currentStep
+function checkMail() {
+    //id tag for for input field for email
+    let myInputEmail = document.getElementById("infoEmail");
+    //the span id error message
+    let errorEmail = document.getElementById("errorEmail");
+    //the regex test for email
+    let myRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function removeSelectors(){
-    selector.forEach(select =>{
-        select.classList.remove("active");
-    })
+    //the input field is a trimmed string
+    if(myInputEmail.value.trim() == ""){
+        //return this error message 
+        errorEmail.innerHTML = "This field is required";
+        //if the value the user puts in fails the regex test
+    } else if(myRegexEmail.test(myInputEmail.value) == false){
+        //return this error message
+        errorEmail.innerHTML = "Enter valid email";
+        else{ 
+            errorEmail.innerHTML = "";
+        }
+    }
 }
-
-function decrementSelector(){
-    removeSelectors();
-    selectorCount--;
-    let select = selector[stepCount];   
-    select.classList.add("active");
-}
-
-function incrementSelector(){
-    removeSelectors();
-    let select = selector[selectorCount];
-    select.classList.add("active");
-    selectorCount++;
-}
-
-function removeStep(){
-    steps.forEach(step =>{
-        step.classList.remove("active");
-    })
-}
-
-
 
 
 
