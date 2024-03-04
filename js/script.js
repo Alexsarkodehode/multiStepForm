304039355a5f1b5e9fde8f6ef9693b736683ebdd
Alexsarkodehode <131833345+Alexsarkodehode@users.noreply.github.com>
2024-03-04 02:52:41 +0100

    Save function reset

--- js/script.js
+++ js/script.js
@@ -5,24 +5,92 @@ function goToStepTwo(){
     Achieve()
 }
 
+
+function reset() {
+    document.getElementById("onlineMonth").checked = false;
+    document.getElementById("storageMonth").checked = false;
+    document.getElementById("customizableMonth").checked = false;
+    document.getElementById("onlineYear").checked = false;
+    document.getElementById("storageYear").checked = false;
+    document.getElementById("customizableYear").checked = false;
+    document.getElementById("onlinePrice").innerHTML = "+0$";
+    document.getElementById("storagePrice").innerHTML = "+0$";
+    document.getElementById("customizablePrice").innerHTML = "+0$";
+    document.getElementById("onlinePrice").innerHTML = "+0$";
+    document.getElementById("storagePrice").innerHTML = "+0$";
+    document.getElementById("customizablePrice").innerHTML = "+0$";
+    document.getElementById("modeResume").innerHTML = "Choose a plan";
+    document.getElementById("priceResume").innerHTML = "0$";
+    monthArcade.style.backgroundColor = "white";
+    monthArcade.style.border = " solid 1px hsl(229, 24%, 87%)";
+    monthPro.style.backgroundColor = "white";
+    monthPro.style.border = " solid 1px hsl(229, 24%, 87%)";
+    monthAdvanced.style.backgroundColor = "white";
+    monthAdvanced.style.border = " solid 1px hsl(229, 24%, 87%)";
+    yearArcade.style.backgroundColor = "white";
+    yearArcade.style.border = " solid 1px hsl(229, 24%, 87%)";
+    yearAdvanced.style.backgroundColor = "white";
+    yearAdvanced.style.border = " solid 1px hsl(229, 24%, 87%)";
+    yearPro.style.backgroundColor = "white";
+    yearPro.style.border = " solid 1px hsl(229, 24%, 87%)";
+  }
+
+// functions for the total price
+
+const planPrice = "0";
+console.log(planPrice);
+
+function getPrice1() {
+  const planPrice = "9";
+  console.log(planPrice);
+  document.getElementById("totalPrice").innerHTML = planPrice;
+}
+
+function getPrice2() {
+  const planPrice = "12";
+  console.log(planPrice);
+  document.getElementById("totalPrice").innerHTML = planPrice;
+}
+function getPrice3() {
+  const planPrice = "15";
+  console.log(planPrice);
+  document.getElementById("totalPrice").innerHTML = planPrice;
+}
+function getPrice4() {
+  const planPrice = "90";
+  console.log(planPrice);
+  document.getElementById("totalPrice").innerHTML = planPrice;
+}
+function getPrice5() {
+  const planPrice = "120";
+  console.log(planPrice);
+  document.getElementById("totalPrice").innerHTML = planPrice;
+}
+function getPrice6() {
+  const planPrice = "150";
+  console.log(planPrice);
+  document.getElementById("totalPrice").innerHTML = planPrice;
+}
+
+//Checking the the inputs in stepInfo
 function checkName(){
     //id tag for for input field for name
-    let myNameInput = document.getElementById("infoName")
+    let myInputName = document.getElementById("infoName")
     //span id "errorName"
     let myNameError = document.getElementById("errorName")
     //
-    let myNameRegex = /^[a-zA-Z-\s]+$/;
+    let myRegexName = /^[a-zA-Z-\s]+$/;
 
     //removing the white parts
-    if(myNameInput.value.trim()== "") {
+    if(myInputName.value.trim()== "") {
         //displays the error message
         myNameError.innerHTML = "This field is required";
         //if the value the user is putting in fails myNameRegex
-    } else if(myNameRegex.test(myNameInput.value)== false){
+    } else if(myRegexName.test(myInputName.value)== false){
         //display this error message
         myNameError.innerHTML = "Please enter a valid name";
         //if the value that the user is putting in isn't a string
-    } else if(myNameInput.value.trim() !== "") {
+    } else if(myInputName.value.trim() !== "") {
         //make it a string
         myNameError.innerHTML = "";
     }
@@ -57,14 +125,14 @@ function checkMail() {
     //the span id error message
     let errorMail = document.getElementById("errorEmail");
     //the regex test for email
-    let myRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let myRegexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     //the input field is a trimmed string
     if(myInputMail.value.trim() == ""){
         //return this error message 
         errorMail.innerHTML = "This field is required";
         //if the value the user puts in fails the regex test
-    } else if(myRegexEmail.test(myInputEmail.value) == false){
+    } else if(myRegexMail.test(myInputEmail.value) == false){
         //return this error message
         errorMail.innerHTML = "Enter valid email";
     }
@@ -73,8 +141,50 @@ function checkMail() {
         }
     }
 
-    functio
+    function Achieve(){
+        let myInputNum = document.getElementById("infoPhoneNumber");
+        let myInputMail = document.createElement("infoMail");
+        let myInputName = document.getElementById("infoName");
 
+        let myRegexName = /^[a-zA-Z-\s]+$/;
+        let myRegexNum = /^[0-9\s]+$/;
+        let myRegexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if(myInputNum.value.trim().length === 10 &&
+           myRegexNum.test(myInputNum.value)== true &&
+           myInputMail.value.trim().length !== 0 &&
+           myRegexMail.test(myInputMail.value)== true &&
+           myInputName.value.trim().length !== 0 &&
+           myRegexName.test(myInputName.value) === true){
+
+            stepInfo.style.display = "none";
+            stepPlan.style.display = "flex";
+            circle1.style.color = "white";
+            circle1.style.backgroundColor = "rgba(255, 255, 255, 0)";
+            circle2.style.backgroundColor = "rgb(255, 255, 255)";
+            circle2.style.color = "black";
+
+        }
+
+    }
+
+    function checkPlan() {
+        if (document.getElementById("modeResume").innerHTML == "Choose a plan") {
+          stepInfo.style.display = "none";
+          stepPlan.style.display = "flex";
+          ring1.style.color = "white";
+          ring1.style.backgroundColor = "rgba(255, 255, 255, 0)";
+          ring2.style.backgroundColor = "rgb(255, 255, 255)";
+          ring2.style.color = "black";
+        } else {
+          stepPlan.style.display = "none";
+          stepAddOn.style.display = "flex";
+          ring2.style.color = "white";
+          ring2.style.backgroundColor = "rgba(255, 255, 255, 0)";
+          ring3.style.backgroundColor = "rgb(255, 255, 255)";
+          ring3.style.color = "black";
+        }
+      }
 
 
 
